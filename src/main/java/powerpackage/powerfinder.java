7fbe50e723250e45182ee58c4f72df113bc20af8
vijaishree <72127806+vijaishree@users.noreply.github.com>
2024-04-13 21:14:18 +0530

    Update powerfinder.java

--- src/main/java/powerpackage/powerfinder.java
+++ src/main/java/powerpackage/powerfinder.java
@@ -1,7 +1,7 @@
 package powerpackage;
 
-public class powerfinder {
-    public static int OF(int xyz, int pqr) {
+public class powerFinder {
+    public static int of(int xyz, int pqr) {
         int p = 1;
         for (int i = 0; i<pqr; i++) {
             p *= xyz;
