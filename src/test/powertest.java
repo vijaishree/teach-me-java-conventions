2dfab2fbea71f8a77e39f7e50e914548f45d1404
vijaishree <72127806+vijaishree@users.noreply.github.com>
2024-04-13 21:11:39 +0530

    Update powertest.java

--- src/test/powertest.java
+++ src/test/powertest.java
@@ -3,24 +3,24 @@ package powerpackage;
 
 import org.junit.Test;
 
-public class powertest {
+public class PowertTest {
     @Test
-    public void one_raised_to_one_is_onew() {
-        assert powerfinder.OF(1,1) == 1;
+    public void oneRaisedToOneIsOne() {
+        assert powerFinder.OF(1,1) == 1;
     }
 
     @Test
-    public void twoRaised_to_one_is_2() {
-        assert powerfinder.OF(2,1) == 2;
+    public void twoRaisedToOneIs2() {
+        assert powerFinder.OF(2,1) == 2;
     }
 
     @Test
-    public void two_power_2_is_4() {
-        assert powerfinder.OF(2, 2) == 4;
+    public void twoPower2Is4() {
+        assert powerFinder.OF(2, 2) == 4;
     }
 
     @Test
-    public void power_of_2_and_3_Is_SIX() {
-        assert powerfinder.OF(3, 2) == 3*3;
+    public void powerOf2And3IsSix() {
+        assert powerFinder.OF(3, 2) == 3*3;
     }
 }
