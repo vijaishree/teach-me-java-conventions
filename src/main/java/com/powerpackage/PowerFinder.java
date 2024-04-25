df3918b0e69340798e516784a6f39c30ecdea758
vijaishree <72127806+vijaishree@users.noreply.github.com>
2024-04-25 14:27:40 +0530

    altered method name

--- src/main/java/com/powerpackage/PowerFinder.java
+++ src/main/java/com/powerpackage/PowerFinder.java
@@ -1,7 +1,7 @@
 package com.powerpackage;
 
 public class PowerFinder {
-    public static int powerCalculator(int base, int power) {
+    public static int find(int base, int power) {
         int powerValue = 1;
         for (int number = 0; number<power; number++) {
             powerValue *= base;
